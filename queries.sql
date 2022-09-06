46e60c4f40345c3822f66715b5eb85d7b749a685
Eikcaj.1998 <jw220998@gmail.com>
2022-09-06 17:23:48 +0200

    es-8

--- queries.sql
+++ queries.sql
@@ -16,17 +16,25 @@ WHERE YEAR(`date_of_birth`) < 1992;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT * 
-    FROM `courses`
-    WHERE `period` = 'i semestre' 
-    AND `year` = 1;
+FROM `courses`
+WHERE `period` = 'i semestre' 
+AND `year` = 1;
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-
+SELECT * 
+FROM `exams`
+WHERE `date` = "2020-06-20"
+AND `hour` BETWEEN "14:00:01" AND "23:59:59";
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
-
-
+SELECT *
+FROM `degrees`
+WHERE `level` = 'magistrale';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(*) AS 'Numero di dipartimenti'
+FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT COUNT(*) AS 'Insegnanti senza numero di telefono'
+FROM `teachers`
+WHERE `teachers`.`phone` IS NULL;
