9c0dec91ae33d6b4b3fd91e7bbe1f5356808cf45
Eikcaj.1998 <jw220998@gmail.com>
2022-09-07 16:03:11 +0200

    join es. 4-5

--- join.sql
+++ join.sql
@@ -14,14 +14,40 @@
     AND `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-    
+     SELECT `teachers`.`id` ,`teachers`.`name` 
+     AS `teacher`, `courses`.`name` 
+     AS `course_name`, `courses`.`year`, `courses`.`period`
+    FROM `courses`
+    JOIN `course_teacher`
+    ON `courses`.`id` = `course_teacher`.`course_id`
+    JOIN `teachers`
+    ON `course_teacher`.`teacher_id` = `teachers`.`id`
+    WHERE `teachers`.`id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea
 -- a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-
+    SELECT *
+    FROM `students`
+    JOIN `degrees`
+    ON `students`.`degree_id` = `degrees`.`id`
+    JOIN `departments`
+    ON `degrees`.`department_id` = `departments`.`id`
+    ORDER BY `students`.`surname`,`students`.`name`;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti 
-
+    SELECT `degrees`.`id`, `degrees`.`name` 
+    AS `degree_name`, `courses`.`name` 
+    AS `course_name`, `teachers`.`id`, `teachers`.`name` 
+    AS `teacher_name`, `teachers`.`surname`  
+    AS `teacher_surname`, `teachers`.`email`  
+    AS `teacher_email` 
+    FROM `degrees`
+    JOIN `courses`
+    ON `degrees`.`id` = `courses`.`degree_id`
+    JOIN `course_teacher`
+    ON `courses`.`id` = `course_teacher`.`course_id`
+    JOIN `teachers`
+    ON `course_teacher`.`teacher_id` = `teachers`.`id`
 
 -- 6. Selezionare tutti i docenti che insegnano nel dipartimento di matematica (54)
 
